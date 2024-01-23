18e426e6431b412a695286bd3b341b80b3707e56
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-23 19:15:17 +0000

    3. API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,8 +11,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={
-            r"/api/v1/*": {"origins": ["http://127.0.0.1:5000", "http://localhost:5000"]}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
